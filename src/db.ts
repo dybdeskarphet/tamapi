025c74ad4931653adf66883d88363852e222d8b6
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-02-16 22:43:57 +0300

    feat(db): Add connectDatabase function

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,24 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import path from "path";
-import { err } from "./helpers";
+import { err, log, ok } from "./helpers";
 
 dotenv.config();
+
+const IDENTIFIER: string = "db";
 
 if (!process.env.MONGODB_URI) {
   err("db", "No database URI specified in .env");
 }
+
+const connectDatabase = async (filename: string) => {
+  try {
+    log(IDENTIFIER, `MongoDB connecting in ${filename}`);
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    ok(IDENTIFIER, `MongoDB connected in ${filename}`);
+  } catch (error) {
+    err(IDENTIFIER, `MongoDB Connection Error:\n ${error}`);
+    process.exit(1); // Exit if connection fails
+  }
+};
+
+export { connectDatabase };
