110ac05f368213f2fa0903a9e43ee13570478605
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-02-16 17:55:10 +0300

    feat(User): Create the model and add password comparison method

---
+++ src/models/User.ts
@@ -0,0 +1,44 @@
+import mongoose, { Document, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import { err } from "../helpers";
+
+const IDENTIFIER: string = "model(User)";
+
+interface IUser extends Document {
+  email: string;
+  name: string;
+  username: string;
+  password: string;
+}
+
+const UserSchema = new mongoose.Schema<IUser>({
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: false },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true, select: false },
+});
+
+UserSchema.pre("save", async function (next) {
+  const user = this as IUser;
+  if (!this.isModified("password")) return next();
+
+  try {
+    // NOTE: Salt creates an extra layer of security
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    user.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error as Error);
+  }
+});
+
+UserSchema.methods.comparePassword = async function (
+  candidatePassword: string,
+): Promise<boolean> {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model("User", UserSchema);
+
+export { User, IUser };
