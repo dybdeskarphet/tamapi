ef75ae037affdbcb9a99aa67c3a5ea95e769c790
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-02-16 23:30:28 +0300

    feat: Add a basic API documentation

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,19 @@ import dotenv from "dotenv";
 import ip from "ip";
 import { ok } from "./helpers";
 import { authRoute } from "./routes/auth";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
 
 dotenv.config();
 const app: Express = express();
+const openapiSpec = YAML.load("./openapi.yaml");
+const port = process.env.API_PORT || 3000;
+openapiSpec.servers = [
+  {
+    url: `http://${ip.address()}:${port}`,
+    description: "Local server",
+  },
+];
 
 // Middleware
 app.use(express.json());
@@ -13,13 +23,14 @@ app.use(express.json());
 // Routes
 app.use("/auth", authRoute);
 
+// Setup API doc
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpec));
+
 // Root
 app.get("/", (req: Request, res: Response) => {
-  res.send("Say hello to tamapi");
+  res.send("Say hello to Tamapi");
   return;
 });
-
-const port = process.env.API_PORT || 3000;
 
 app.listen(port, () => {
   ok("server", `Server running on http://${ip.address()}:${port}`);
