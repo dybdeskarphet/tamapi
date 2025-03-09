1440f3d35663e0b7a412f461032f49849eb68344
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-03-09 03:57:14 +0300

    feat(Pet): Add min-max validators for relevant fields

--- src/models/Pet.ts
+++ src/models/Pet.ts
@@ -11,8 +11,6 @@ enum PetType {
   Alien = "Alien",
 }
 
-type PetTypeEnum = `${PetType}`;
-
 interface IPet extends Document {
   name: string;
   type: PetType;
@@ -26,14 +24,57 @@ interface IPet extends Document {
 }
 
 const PetSchema = new mongoose.Schema<IPet>({
-  name: { type: String, required: true },
-  type: { type: String, enum: Object.values(PetType), required: true },
-  age: { type: Number, required: true },
-  health: { type: Number, required: true },
-  happiness: { type: Number, required: true },
-  hunger: { type: Number, required: true },
-  energy: { type: Number, required: true },
-  hygiene: { type: Number, required: true },
+  name: {
+    type: String,
+    required: true,
+    minlength: [1, "Pet must have a name"],
+  },
+  type: {
+    type: String,
+    enum: Object.values(PetType),
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+    default: 1,
+    min: [1, "Age must be at least 1"],
+  },
+  health: {
+    type: Number,
+    required: true,
+    default: 100,
+    min: [0, "Health must be at least 0"],
+    max: [100, "Health must be lower than 100"],
+  },
+  happiness: {
+    type: Number,
+    required: true,
+    default: 100,
+    min: [0, "Happiness must be at least 0"],
+    max: [100, "Happiness must be lower than 100"],
+  },
+  hunger: {
+    type: Number,
+    required: true,
+    default: 100,
+    min: [0, "Hunger must be at least 0"],
+    max: [100, "Hunger must be lower than 100"],
+  },
+  energy: {
+    type: Number,
+    required: true,
+    default: 100,
+    min: [0, "Energy must be at least 0"],
+    max: [100, "Energy must be lower than 100"],
+  },
+  hygiene: {
+    type: Number,
+    required: true,
+    default: 100,
+    min: [0, "Hygiene must be at least 0"],
+    max: [100, "Hygiene must be lower than 100"],
+  },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
