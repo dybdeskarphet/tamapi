29f9ca60f95333c5e70efbf7005d9980f304b7b7
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-03-09 04:36:32 +0300

    fix(Pet): Delete requirements for the fields that have a default value

--- src/models/Pet.ts
+++ src/models/Pet.ts
@@ -36,46 +36,40 @@ const PetSchema = new mongoose.Schema<IPet>({
   },
   age: {
     type: Number,
-    required: true,
     default: 1,
     min: [1, "Age must be at least 1"],
   },
   health: {
     type: Number,
-    required: true,
     default: 100,
     min: [0, "Health must be at least 0"],
     max: [100, "Health must be lower than 100"],
   },
   happiness: {
     type: Number,
-    required: true,
     default: 100,
     min: [0, "Happiness must be at least 0"],
     max: [100, "Happiness must be lower than 100"],
   },
   hunger: {
     type: Number,
-    required: true,
     default: 100,
     min: [0, "Hunger must be at least 0"],
     max: [100, "Hunger must be lower than 100"],
   },
   energy: {
     type: Number,
-    required: true,
     default: 100,
     min: [0, "Energy must be at least 0"],
     max: [100, "Energy must be lower than 100"],
   },
   hygiene: {
     type: Number,
-    required: true,
     default: 100,
     min: [0, "Hygiene must be at least 0"],
     max: [100, "Hygiene must be lower than 100"],
   },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
 const Pet = mongoose.model("Pet", PetSchema);
