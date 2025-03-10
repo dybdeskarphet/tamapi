b7b486e205a909f3b0d1c3d9ced278ad626dbb7a
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-03-10 15:20:40 +0300

    feat(auth): Connect developer access function with its route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  devAccess,
   getAuthRoot,
   postAuthLogin,
   postAuthRegister,
@@ -9,5 +10,6 @@ const router = express.Router();
 router.get("/", getAuthRoot);
 router.post("/register", postAuthRegister);
 router.post("/login", postAuthLogin);
+router.post("/dev-access", devAccess);
 
 export { router as authRoute };
