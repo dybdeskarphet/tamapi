d6355351ebab81722450363a1dcc87cf5ebf8e79
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-03-10 03:34:21 +0300

    feat(routes): Add getting pet by ID and creating new pet

--- src/routes/pets.ts
+++ src/routes/pets.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { getPets } from "../controllers/PetsController";
+import { getPet, getPets, postPet } from "../controllers/PetsController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 router.get("/", authMiddleware, getPets);
+router.get("/:id", authMiddleware, getPet);
+router.post("/", authMiddleware, postPet);
 
 export { router as petsRoute };
