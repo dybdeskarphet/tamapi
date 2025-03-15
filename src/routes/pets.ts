5aeeb4fcd79196eb68643c5efdcc6d726522e42c
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-03-15 04:12:01 +0300

    feat(pets): Add pet feeding endpoint

--- src/routes/pets.ts
+++ src/routes/pets.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { getPet, getPets, postPet } from "../controllers/PetsController";
+import {
+  feedPet,
+  getPet,
+  getPets,
+  postPet,
+} from "../controllers/PetsController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 router.get("/", authMiddleware, getPets);
 router.get("/:id", authMiddleware, getPet);
 router.post("/", authMiddleware, postPet);
+router.post("/:id/feed", authMiddleware, feedPet);
 
 export { router as petsRoute };
