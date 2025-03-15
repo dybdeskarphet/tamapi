17b3ee04a02f2917d32e0105eb22b2fef0d1abeb
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-03-15 16:56:34 +0300

    feat+refactor: Add pet sleeping endpoint and change route names

--- src/routes/petsRoutes.ts
+++ src/routes/petsRoutes.ts
@@ -4,6 +4,7 @@ import {
   getPet,
   getPets,
   postPet,
+  sleepPet,
 } from "../controllers/PetsController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
@@ -12,5 +13,6 @@ router.get("/", authMiddleware, getPets);
 router.get("/:id", authMiddleware, getPet);
 router.post("/", authMiddleware, postPet);
 router.post("/:id/feed", authMiddleware, feedPet);
+router.post("/:id/sleep", authMiddleware, sleepPet);
 
 export { router as petsRoute };
--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import dotenv from "dotenv";
 import express, { Express } from "express";
 import ip from "ip";
 import { ok } from "./helpers";
-import { authRoute } from "./routes/auth";
+import { authRoute } from "./routes/authRoutes";
 import YAML from "yamljs";
 import swaggerUi from "swagger-ui-express";
 import { rootController } from "./controllers/RootController";
-import { petsRoute } from "./routes/pets";
+import { petsRoute } from "./routes/petsRoutes";
 import { connectDatabase } from "./db";
 import path from "path";
 
