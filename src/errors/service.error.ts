0bf43896b3055d2980bc5b099a04671843b8df25
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-04-02 03:52:08 +0300

    feat(service.error): Add errors field for additional information about errors

--- src/errors/service.error.ts
+++ src/errors/service.error.ts
@@ -1,8 +1,10 @@
 class ServiceError extends Error {
   status: number;
-  constructor(status: number, message: string) {
+  errors: object | null;
+  constructor(status: number, message: string, errors: object | null = null) {
     super(message);
     this.status = status;
+    this.errors = errors && Object.keys(errors).length > 0 ? errors : null;
     //  Keep the below code for historical reasons.
     Object.setPrototypeOf(this, new.target.prototype);
   }
