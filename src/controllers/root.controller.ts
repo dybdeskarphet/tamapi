90be0bb76c7e8dbd1c35dad51049a437a6fcf35d
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-04-16 02:03:10 +0300

    feat: add new root page

--- src/controllers/root.controller.ts
+++ src/controllers/root.controller.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express";
+import path from "path";
+import fs from "fs";
 
 const rootController = (req: Request, res: Response) => {
-  res.send("Say hello to Tamapi");
-  return;
+  const filePath = path.join(__dirname, "../../public/index.html");
+
+  // Read the HTML file and send it as a response
+  fs.readFile(filePath, "utf8", (err, htmlData) => {
+    if (err) {
+      res.status(500).send("Error loading homepage.");
+      return;
+    }
+    res.setHeader("Content-Type", "text/html");
+    res.send(htmlData);
+  });
 };
 
 export { rootController };
